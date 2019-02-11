0b267c3fdeb974d902c3e0b8580b472f893bea2b
Admin <ponomarenko02041993@gmail.com>
2019-02-11 21:12:22 +0200

    new

--- js/main.js
+++ js/main.js
@@ -62,3 +62,16 @@ $(document).ready(function () {
     });
 
 });
+
+jQuery("#backtotop").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 0
+    }, 600);
+});
+jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 450) {
+        jQuery("#backtotop").addClass("visible");
+    } else {
+        jQuery("#backtotop").removeClass("visible");
+    }
+});
