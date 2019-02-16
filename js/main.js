3d5a7228545815b81dcdef6dbb39032fecbb873e
Admin <ponomarenko02041993@gmail.com>
2019-02-16 12:07:51 +0200

    new

--- js/main.js
+++ js/main.js
@@ -7,54 +7,55 @@ $(document).ready(function () {
         });
     });
 
-    jQuery('#vmap').vectorMap({
-        map: 'world_en',
-        backgroundColor: '#ffffff',
-        color: '#ffffff',
-        selectedColor: '#3f6c3e',
-        borderColor: '#ffffff',
-        enableZoom: true,
-        showTooltip: true,
-        scaleColors: ['#eaeaea'],
-        values: sample_data,
-        normalizeFunction: 'polynomial',
-        onRegionClick: function(event, code) {
-            if (code == "ua") {
-                $(".open-modal").addClass("open-pop");
-                $('body').addClass('overflow');
+    if (jQuery('#vmap').length){
+        jQuery('#vmap').vectorMap({
+            map: 'world_en',
+            backgroundColor: '#ffffff',
+            color: '#ffffff',
+            selectedColor: '#3f6c3e',
+            borderColor: '#ffffff',
+            enableZoom: true,
+            showTooltip: true,
+            scaleColors: ['#eaeaea'],
+            values: sample_data,
+            normalizeFunction: 'polynomial',
+            onRegionClick: function(event, code) {
+                if (code == "ua") {
+                    $(".open-modal").addClass("open-pop");
+                    $('body').addClass('overflow');
+                }
+                if (code == "US") {
+                    label.text('Bears, vodka, balalaika');
+                }
+                if (code == "DE") {
+                    window.location = '/url4'
+                }
+                if (code == "IE") {
+                    window.location = '/url5'
+                }
             }
-            if (code == "US") {
-                label.text('Bears, vodka, balalaika');
-            }
-            if (code == "DE") {
-                window.location = '/url4'
-            }
-            if (code == "IE") {
-                window.location = '/url5'
-            }
-        }
-        
-    });
-    let countriesAbbr = [
-        'us','ca','mx','ru','fi','se','no',
-        'gb','ie','fr','es','dk','nl', 'de',
-        'pt','be','ee','lv','lt','by','pl','cz',
-        'at','ua','it','sk','hu','ro','bg','si',
-        'hr','gr'
-    ];
 
-    countriesAbbr.forEach(function (abbr) {
-        jQuery('#vmap').vectorMap('set', 'colors', {[abbr]: '#9cbb3e'});
-    });
+        });
+        let countriesAbbr = [
+            'us','ca','mx','ru','fi','se','no',
+            'gb','ie','fr','es','dk','nl', 'de',
+            'pt','be','ee','lv','lt','by','pl','cz',
+            'at','ua','it','sk','hu','ro','bg','si',
+            'hr','gr'
+        ];
 
-    let countriesAbbr2 =[
-       'gl','cu'
-    ];
+        countriesAbbr.forEach(function (abbr) {
+            jQuery('#vmap').vectorMap('set', 'colors', {[abbr]: '#9cbb3e'});
+        });
 
-    countriesAbbr2.forEach(function (abbr2) {
-        jQuery('#vmap').vectorMap('set', 'colors', {[abbr2]: '#eaeaea'});
-    });
+        let countriesAbbr2 =[
+            'gl','cu'
+        ];
 
+        countriesAbbr2.forEach(function (abbr2) {
+            jQuery('#vmap').vectorMap('set', 'colors', {[abbr2]: '#eaeaea'});
+        });
+    }
 
     $(".close-btn").click(function () {
         $(".open-modal").removeClass("open-pop");
